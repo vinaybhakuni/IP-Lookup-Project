98ac8878125c5d1ec48c5026f276d55cc6f43af1
vinaybhakuni <95526403+vinaybhakuni@users.noreply.github.com>
2022-08-22 20:28:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const lookupBtn = document.querySelector(".lookup-btn");
+const ipDisplay = document.querySelector(".ip-display");
+const locationDisplay = document.querySelector(".location-display");
+const geoDisplay = document.querySelector(".geo-display");
+const loader = document.querySelector(".loader-container");
+const details = document.querySelector(".details");
+
+lookupBtn.addEventListener("click", () => {
+  loader.style.display = "flex";
+  details.style.display = "none";
+
+  axios.get("https://ipapi.co/json/").then(response =>{
+    loader.style.display = "none";
+    details.style.display = "block";
+  ipDisplay.textContent = `IP Address: - ${response.data.ip}`;
+    
+    locationDisplay.textContent = `Location: - ${response.data.city}, ${response.data.region}, ${response.data.country_name}`;
+
+    geoDisplay.textContent = `Geo Location: - ${response.data.latitude}, ${response.data.longitude}`;
+  });
+});
